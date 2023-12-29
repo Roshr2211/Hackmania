a403a088180ec8883582a26ddcfe3cf8489ce4da
Rosh <roshniraju700@gmail.com>
2023-12-29 11:12:39 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// src/App.jsx
+
+import React from 'react';
+//import Home from './components/Home';
+import Home from './pages/Home/Home1.';
+//import Home from './pages/Home/Home.jsx';
+
+const App = () => {
+  return (
+    <div>
+      {/* You can include a header or navigation bar here if needed */}
+      <Home />
+      {/* You can include a footer or other components here if needed */}
+    </div>
+  );
+};
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
+// src/index.js or src/index.jsx
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
