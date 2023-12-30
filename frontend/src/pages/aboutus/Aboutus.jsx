6118dfa2c29950b6ea78ba5f4f731c75ac5c872d
Rosh <roshniraju700@gmail.com>
2023-12-31 00:49:17 +0530

    About us added

---
+++ frontend/src/pages/aboutus/Aboutus.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './aboutus.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our study group website! We believe in the power of collaboration and shared knowledge.
+        Our platform is designed to connect students, foster teamwork on assignments and projects, and enhance
+        academic excellence.
+      </p>
+      <p>
+        Join us in this collective journey of learning, where minds unite, and potential is unleashed.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
